91ef4982ea6c107e8e088d106a2c1694f6236eb4
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-06-04 22:06:58 -0300

    20

---
+++ script.js
@@ -0,0 +1,69 @@
+/* ################ EJERCICIO 0 #########################*/
+
+// let nombre = prompt("Ingrese su nombre");
+// console.log(nombre);
+// alert("Hola" + " " + (nombre));
+// let apellido = prompt("Ingrese su apellido");
+// alert("Hola" + " " + (nombre) + " " + (apellido));
+
+
+
+
+
+/* ################ EJERCICIO 1 #########################*/
+
+// let numeroA = prompt("Ingrese un número");
+// let numeroB = prompt ("Ingrese otro número"); 
+// console.log(parseInt(numeroA) + parseInt(numeroB));
+
+
+
+
+
+/* ################ EJERCICIO 2 #########################*/
+
+// let age = prompt("Cuántos años cumple o cumplirá este año");
+// console.log("Usted nació en" + " " + (parseInt(2021)-parseInt(age)));
+
+
+
+
+
+
+/* ################ EJERCICIO 3 #########################*/
+
+// let balance = 10000.59;
+// alert("Usted tiene en su cuenta" + " " + (balance));
+// let extraccion = prompt("Ingrese el monto que desde retirar");
+// alert("Su nuevo balance es de" + " " + (parseFloat(balance) - parseFloat(extraccion)));
+
+
+
+
+
+/* ################ EJERCICIO 4 #########################*/
+
+// let nota1 = prompt("Ingrese la primer nota");
+// let nota2 = prompt("Ingrese la segunda nota");
+// let nota3 = prompt("Ingrese la tercer nota");
+// let promedio = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / 3;
+// console.log(parseInt(promedio));
+
+
+
+
+
+
+/* ################ EJERCICIO 5 #########################*/
+
+let valorMetro = prompt("Por favor ingrese el valor que desea convertir a metros");
+let pies = parseFloat(valorMetro) * parseFloat(3.28);
+let cms = parseFloat(valorMetro) * parseInt(100);
+let pulgadas = parseFloat(valorMetro) * parseFloat(39.38);
+console.log((pies) + " " + "ft");
+console.log((cms) + " " + "cms");
+console.log((pulgadas) + " " + "in");
+
+
+
+
